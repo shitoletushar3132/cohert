81797a917b5884686a9ef39e38f4960c4dd306df
shitoletushar3132 <shitoletushar3132@gmail.com>
2024-12-13 23:10:27 +0530

    api context

--- week 7/routing/src/App.jsx
+++ week 7/routing/src/App.jsx
@@ -1,30 +1,55 @@
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Landing from "./components/Landing";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useContext, useState, createContext } from "react";
+
 const Dashboard = lazy(() => import("./components/Dashboard"));
+
+export const CountContext = createContext();
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
+    <CountContext.Provider value={{ count, setCount }}>
       <BrowserRouter>
         <Appbar />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route
-            path="/landing"
+            path="/dashboard"
             element={
-              <Suspense fallback={<>.....</>}>
+              <Suspense fallback={<div>Loading Dashboard...</div>}>
                 <Dashboard />
               </Suspense>
             }
           />
         </Routes>
+        <Display />
       </BrowserRouter>
+    </CountContext.Provider>
+  );
+}
+
+function Display() {
+  return (
+    <div>
+      <CountRenderer />
+      <Button />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const { count } = useContext(CountContext);
+  return <h1>{count}</h1>;
+}
+
+function Button() {
+  const { count, setCount } = useContext(CountContext);
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </>
   );
 }
@@ -34,22 +59,8 @@ function Appbar() {
 
   return (
     <div>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Landing
-        </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-        >
-          Dashboard
-        </button>
-      </div>
+      <button onClick={() => navigate("/")}>Landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
     </div>
   );
 }
