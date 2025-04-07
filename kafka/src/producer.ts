f35fc128ceb43f9ec7d6669170213b18b817f896
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-04-07 21:54:25 +0530

    add

---
+++ kafka/src/producer.ts
@@ -0,0 +1,17 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+const run = async () => {
+  // Producing
+  await producer.connect();
+  await producer.send({
+    topic: "quickstart-events",
+    messages: [{ value: "Hello KafkaJS user! from nodejs" }],
+  });
+};
